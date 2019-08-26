892fe54e7c13944eee089f1d6fc011d004662b1a
Akshayguatam144 <akshay.gautam144@gmail.com>
2019-08-26 17:54:30 +0530

    Commit the whole Project

---
+++ src/java/Constants.java
@@ -0,0 +1,11 @@
+package java;
+
+
+public interface Constants 
+{
+   String CHROMEKEY="webdriver.chrome.driver";
+   String CHROMEVALUE="./driver/chromedriver.exe";
+   String URL="http://www.gmail.com/";
+   String USERNAME="mugenzaraki@gmail.com";
+   String PASSWORD="kenpachizaraki";
+}
